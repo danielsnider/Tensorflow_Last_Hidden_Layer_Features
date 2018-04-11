68a765b07fd1b1efa707bc50e5a95e47478583a6
danielsnider <danielsnider12@gmail.com>
2018-04-10 20:06:38 -0400

    Working resnet and vgg16

--- train_neck_data.py
+++ train_neck_data.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2' # disables warning: Your CPU supports instructions that this TensorFlow binary was not compiled to use: SSE4.1 SSE4.2 AVX AVX2 FMA
+
 import time
 import glob
 import skimage
@@ -8,6 +11,7 @@ import numpy as np
 import mahotas as mh
 import tensorflow as tf
 import tensorflow.contrib.slim.nets as nets
+from tensorflow.contrib.slim.nets import resnet_v2
 
 from PIL import Image
 from IPython import embed
@@ -19,7 +23,7 @@ vgg = nets.vgg
 
 # Load images
 filenames=list(glob.glob('./images/*.jpg'))
-images = np.zeros((len(filenames), 224, 224, 3), dtype=np.float16)
+images = np.zeros((len(filenames), 224, 224, 3), dtype=np.float32)
 for i, imageName in enumerate(filenames): 
   print i, imageName
   img = skimage.io.imread(imageName)
@@ -30,36 +34,82 @@ for i, imageName in enumerate(filenames):
   img = scipy.misc.imresize(img, (224,224))
   images[i,:,:,:] = img
 
-in_images = tf.placeholder(images.dtype, images.shape)
+# in_images = tf.placeholder(tf.half, images.shape)
+in_images = tf.placeholder(tf.float32, images.shape)
 
-# Initialize model
-model, intermed = vgg.vgg_16(in_images)
-fc7 = intermed['vgg_16/fc7']
+
 
 # More info https://github.com/machrisaa/tensorflow-vgg/blob/master/vgg16.py
 # maybe useful get_fc_weight
 
-# Initialize weights restore
-img_net_path = '/home/dan/tensorflow_tests/vgg_weights/vgg_16.ckpt'
-restored_variables = tf.contrib.framework.get_variables_to_restore()
-restorer = tf.train.Saver(restored_variables)
+# # Initialize weights restore
+img_net_path = '/home/dan/tensorflow_tests/models/vgg_16.ckpt'
 
-# embed()
 
-with tf.Session() as sess:
-  restorer.restore(sess, img_net_path)
-  # ^ error
+# Initialize model
+# model, intermed = vgg.vgg_16(in_images)
+# model, intermed = vgg.vgg_16(in_images, num_classes=2, is_training=False)
 
-  sess.run(fc7, feed_dict={in_images:images})
+with slim.arg_scope(vgg.vgg_arg_scope()):
+  model, intermed = vgg.vgg_16(in_images)
 
-  # merged_summaries = tf.summary.merge_all()
-  # writer = tf.summary.FileWriter('/home/yasaman/HN/run_log/', sess.graph)
-  # for i in range(5000):
-  #   if(i%100 == 0):
-  #     summ = sess.run(merged_summaries)
-  #     writer.add_summary(summ, i)
-  #     print(sess.run(total_loss))
-  #     saver.save(sess, us_path)
-  # writer.close()
+   # net, end_points = resnet_v2.resnet_v2_101(inputs, 1000, is_training=False)
+  # model, intermed = resnet_v2.resnet_v2_101(in_images, 1001, is_training=False)
 
+  fc7 = intermed['vgg_16/fc7']
+  # conv1_1 = intermed['vgg_16/conv1/conv1_1']
+  # conv1_1 = tf.cast(conv1_1, tf.half)
+
+  # embed()
+
+  # restored_variables = tf.contrib.framework.get_variables_to_restore(exclude=exclude_layers)
+  restored_variables = tf.contrib.framework.get_variables_to_restore()
+  restorer = tf.train.Saver(restored_variables)
+
+
+
+  with tf.Session() as sess:
+    restorer.restore(sess, img_net_path)
+    # ^ error
+
+    features = sess.run(fc7, feed_dict={in_images:images})
+    # features = sess.run(model, feed_dict={in_images:images})
+
+    embed()
+
+    # merged_summaries = tf.summary.merge_all()
+    # writer = tf.summary.FileWriter('/home/yasaman/HN/run_log/', sess.graph)
+    # for i in range(5000):
+    #   if(i%100 == 0):
+    #     summ = sess.run(merged_summaries)
+    #     writer.add_summary(summ, i)
+    #     print(sess.run(total_loss))
+    #     saver.save(sess, us_path)
+    # writer.close()
+
+
+
+  # In [2]: intermed.keys()
+  # Out[2]: 
+  # ['vgg_16/conv1/conv1_1',
+  #  'vgg_16/conv1/conv1_2',
+  #  'vgg_16/pool1',
+  #  'vgg_16/conv2/conv2_1',
+  #  'vgg_16/conv2/conv2_2',
+  #  'vgg_16/pool2',
+  #  'vgg_16/conv3/conv3_1',
+  #  'vgg_16/conv3/conv3_2',
+  #  'vgg_16/conv3/conv3_3',
+  #  'vgg_16/pool3',
+  #  'vgg_16/conv4/conv4_1',
+  #  'vgg_16/conv4/conv4_2',
+  #  'vgg_16/conv4/conv4_3',
+  #  'vgg_16/pool4',
+  #  'vgg_16/conv5/conv5_1',
+  #  'vgg_16/conv5/conv5_2',
+  #  'vgg_16/conv5/conv5_3',
+  #  'vgg_16/pool5',
+  #  'vgg_16/fc6',
+  #  'vgg_16/fc7',
+  #  'vgg_16/fc8']
 
